510efa4ba5952809576b656720378ec01f09d1cc
Jonathan Lau <me@jon.sg>
2016-10-07 00:09:22 +0800

    Add porter request.

--- app/src/main/java/com/percimal/singaporeairlines/UberRequestActivity.java
+++ app/src/main/java/com/percimal/singaporeairlines/UberRequestActivity.java
@@ -1,11 +1,14 @@
 package com.percimal.singaporeairlines;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.Toast;
 
 import com.uber.sdk.android.core.UberSdk;
 import com.uber.sdk.android.core.auth.AccessTokenManager;
@@ -15,16 +18,29 @@ import com.uber.sdk.android.core.auth.LoginManager;
 import com.uber.sdk.core.auth.AccessToken;
 import com.uber.sdk.core.auth.Scope;
 import com.uber.sdk.rides.client.SessionConfiguration;
+import com.uber.sdk.rides.client.UberRidesApi;
+import com.uber.sdk.rides.client.model.Ride;
+import com.uber.sdk.rides.client.services.RidesService;
 
 import java.util.Arrays;
 
-public class UberRequestActivity extends AppCompatActivity implements UberRequestFragment.OnFragmentInteractionListener {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+public class UberRequestActivity extends AppCompatActivity implements
+        UberAuthFragment.OnFragmentInteractionListener,
+        UberRequestFragment.OnFragmentInteractionListener,
+        PorterRequestDialogFragment.OnFragmentInteractionListener {
+
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 1;
     AccessTokenManager accessTokenManager;
     LoginManager loginManager;
     Flight flight;
     UberAuthFragment uberAuthFragment;
     UberRequestFragment uberRequestFragment;
+    PorterRequestDialogFragment porterRequestDialog;
+    RidesService ridesService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +51,15 @@ public class UberRequestActivity extends AppCompatActivity implements UberReques
 
         flight = (Flight) getIntent().getSerializableExtra("flight");
 
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            initializeActivity();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+
+    }
+
+    private void initializeActivity() {
         SessionConfiguration config = new SessionConfiguration.Builder()
                 .setClientId(getString(R.string.uber_client_id))
                 .setClientSecret(getString(R.string.uber_client_secret))
@@ -56,9 +81,7 @@ public class UberRequestActivity extends AppCompatActivity implements UberReques
 
             @Override
             public void onLoginSuccess(@NonNull AccessToken accessToken) {
-                uberRequestFragment = new UberRequestFragment();
-                uberRequestFragment.setArguments(getIntent().getExtras());
-                getFragmentManager().beginTransaction().replace(R.id.frame_layout, uberRequestFragment).commit();
+                uberTokenInit();
             }
 
             @Override
@@ -71,23 +94,93 @@ public class UberRequestActivity extends AppCompatActivity implements UberReques
         loginManager = new LoginManager(accessTokenManager, loginCallback);
 
         if(loginManager.isAuthenticated()) {
-            uberRequestFragment = new UberRequestFragment();
-            uberRequestFragment.setArguments(getIntent().getExtras());
-            getFragmentManager().beginTransaction().add(R.id.frame_layout, uberRequestFragment).commit();
+            uberTokenInit();
         } else {
             uberAuthFragment = new UberAuthFragment();
             getFragmentManager().beginTransaction().add(R.id.frame_layout, uberAuthFragment).commit();
         }
 
+        porterRequestDialog = new PorterRequestDialogFragment();
     }
 
-    public void uberLogin(View view) {
-        loginManager.login(this);
+    private void uberTokenInit() {
+        uberRequestFragment = new UberRequestFragment();
+        uberRequestFragment.setArguments(getIntent().getExtras());
+        getFragmentManager().beginTransaction().replace(R.id.frame_layout, uberRequestFragment).commit();
+        ridesService = UberRidesApi.with(loginManager.getSession()).build().createService();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
         loginManager.onActivityResult(this, requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_FINE_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initializeActivity();
+                } else {
+                    Toast.makeText(this, "This feature requires location permission", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void uberLogin() {
+        loginManager.login(this);
+    }
+
+    @Override
+    public void showPorterRequestDialog() {
+        porterRequestDialog.show(getFragmentManager(), "porter");
+        ridesService.getCurrentRide().enqueue(new Callback<Ride>() {
+            @Override
+            public void onResponse(Call<Ride> call, Response<Ride> response) {
+                if (response.isSuccessful()) {
+                    Ride ride = response.body();
+                    // TODO: Query backend to check if ride.getRideId() already requested for porter service.
+                    boolean porterRequested = false;
+                    if (porterRequested) {
+                        porterRequestDialog.initialize(PorterRequestDialogFragment.PorterState.REQUESTED);
+                    } else {
+                        porterRequestDialog.initialize(PorterRequestDialogFragment.PorterState.AVAILABLE);
+                    }
+                } else {
+                    porterRequestDialog.initialize(PorterRequestDialogFragment.PorterState.UNAVAILABLE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Ride> call, Throwable t) {
+                porterRequestDialog.initialize(PorterRequestDialogFragment.PorterState.UNAVAILABLE);
+            }
+        });
+    }
+
+    @Override
+    public void requestPorter() {
+        ridesService.getCurrentRide().enqueue(new Callback<Ride>() {
+            @Override
+            public void onResponse(Call<Ride> call, Response<Ride> response) {
+                if (response.isSuccessful()) {
+                    Ride ride = response.body();
+                    // TODO: Send request to track `ride.getRideId()`.
+                    porterRequestDialog.requestCallback(true);
+                } else {
+                    porterRequestDialog.requestCallback(false);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Ride> call, Throwable t) {
+                porterRequestDialog.requestCallback(false);
+            }
+        });
     }
 }
--- app/src/main/java/com/percimal/singaporeairlines/UberRequestFragment.java
+++ app/src/main/java/com/percimal/singaporeairlines/UberRequestFragment.java
@@ -4,30 +4,28 @@ import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
-import com.google.android.gms.location.places.ui.PlacePicker;
 import com.uber.sdk.android.rides.RideParameters;
 import com.uber.sdk.android.rides.RideRequestView;
 
 
 public class UberRequestFragment extends Fragment {
 
+    private static final int PORTER_MENU_ITEM = 1;
     private OnFragmentInteractionListener mListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         View view = inflater.inflate(R.layout.fragment_uber_request, container, false);
         RideParameters rideParams = new RideParameters.Builder()
-                .setDropoffLocation(1.356673, 103.988000, "Terminal 3 Depature", "Airport Blvd")
+                .setDropoffLocation(1.356673, 103.988000, "Terminal 3 Departure", "Airport Blvd")
                 .build();
         RideRequestView rideRequestView = (RideRequestView) view.findViewById(R.id.ride_request_view);
         rideRequestView.setRideParameters(rideParams);
@@ -41,11 +39,9 @@ public class UberRequestFragment extends Fragment {
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
         }
     }
-
 
     @Override
     public void onDetach() {
@@ -53,7 +49,26 @@ public class UberRequestFragment extends Fragment {
         mListener = null;
     }
 
-    public interface OnFragmentInteractionListener {
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        MenuItem menuItem = menu.add(Menu.NONE, PORTER_MENU_ITEM, Menu.NONE, "Porter Service");
+        menuItem.setIcon(R.drawable.ic_porter);
+        menuItem.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case PORTER_MENU_ITEM:
+                mListener.showPorterRequestDialog();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public interface OnFragmentInteractionListener {
+        void showPorterRequestDialog();
     }
 }
